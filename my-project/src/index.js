9f0255fc7ce5a12fff91feef09a8566cf5781ce0
Harminder-Pal <85278472+Harminder-Pal@users.noreply.github.com>
2021-06-03 16:01:01 +0530

    Add files via upload

---
+++ my-project/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import MyApp from "./App";
+//import styles from "index.css";
+//import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/*<MyForm /> */}
+    <MyApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+//reportWebVitals();
